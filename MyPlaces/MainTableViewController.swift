f85177e340f6cc41eee26d9afc0f8614f081e337
VLADISLAV <SIZONOV.95@MAIL.RU>
2021-09-30 15:44:52 +0300

    setup appearance of new place ViewController

--- MyPlaces/MainTableViewController.swift
+++ MyPlaces/MainTableViewController.swift
@@ -9,7 +9,9 @@ import UIKit
 
 class MainTableViewController: UITableViewController {
 
-    let restaurantNames = ["Burger Heroes", "Kitchen","Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+//    let restaurantNames = ["Burger Heroes", "Kitchen","Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Sherlock Holmes", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
+    
+    let places = [Place(name: "Burger Heroes", location: "Moscow", type: "Кафе", image: "Burger Heroes")]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,15 +20,17 @@ class MainTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        restaurantNames.count
+        places.count
     }
 
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! CustomTableViewCell
 
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.imageOfPlace.image = UIImage(named: restaurantNames[indexPath.row])
+        cell.nameLabel.text = places[indexPath.row].name
+        cell.locationLabel.text = places[indexPath.row].location
+        cell.typeLabel.text = places[indexPath.row].type
+        cell.imageOfPlace.image = UIImage(named: places[indexPath.row].image)
         cell.imageOfPlace.layer.cornerRadius = cell.imageOfPlace.frame.size.height / 2
         cell.imageOfPlace.clipsToBounds = true
         
@@ -47,5 +51,7 @@ class MainTableViewController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
+    
+    @IBAction func cancelAction(_ seque: UIStoryboardSegue) {}
 
 }
---
+++ MyPlaces/PlaceModel.swift
@@ -0,0 +1,16 @@
+//
+//  PlaceModel.swift
+//  MyPlaces
+//
+//  Created by Владислав Сизонов on 30.09.2021.
+//
+
+import Foundation
+
+struct  Place {
+    
+    var name: String
+    var location: String
+    var type: String
+    var image: String
+}
